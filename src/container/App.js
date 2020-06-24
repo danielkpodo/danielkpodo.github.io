2763f476076e20a4ea98dbc3db2c59f6c328aa29
danielkpodo <kpododanielnarh@gmail.com>
2020-06-24 12:37:35 +0000

    set-up app folders and files structure

---
+++ src/container/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Headroom from "react-headroom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Headroom>
+        <h1>Hello World!</h1>
+      </Headroom>
+    </div>
+  );
+}
+
+export default App;
