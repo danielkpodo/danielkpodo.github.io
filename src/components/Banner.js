1c899eb6ff442a7f0e3f54638cac693e8ce6b924
danielkpodo <kpododanielnarh@gmail.com>
2020-06-24 22:52:39 +0000

    added programmer illustrations

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
+import narh from "./narh.svg";
 
 const Banner = () => {
   return (
     <div className="container banner">
       <div className="row">
-        <div className="col l6">
+        <div className="col s12 l6">
           <h1>
             Hello, I'm Narh{" "}
             <span role="img" aria-label="emoji hand">
@@ -12,15 +13,17 @@ const Banner = () => {
             </span>
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            voluptatem facere vitae temporibus excepturi veritatis! At
-            architecto, provident voluptates veniam aspernatur obcaecati. Vel,
-            illo maxime, sapiente dolorum quam magni ipsa assumenda eos sunt
-            voluptates non eaque quos, architecto quisquam dolorem quaerat iure
-            quibusdam officiis earum! Saepe suscipit ut illo consectetur.
+            Make an api call to github to fetch about Rerum voluptatem facere
+            vitae temporibus excepturi veritatis! At architecto, provident
+            voluptates veniam aspernatur obcaecati. Vel, illo maxime, sapiente
+            dolorum quam magni ipsa assumenda eos sunt voluptates non eaque
+            quos, architecto quisquam dolorem quaerat iure quibusdam officiis
+            earum! Saepe suscipit ut illo consectetur.
           </p>
         </div>
-        <div className="col l6">Image here</div>
+        <div className="col s12 l6">
+          <img src={narh} alt="programmer coding" className="programmer-img" />
+        </div>
       </div>
     </div>
   );
